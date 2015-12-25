8f80deb6e972f6eda896d26bf848bf55a32c837e
Joe Maples <frap129@gmail.com>
2015-12-25 18:19:59 -0500

    runtime: Fix compile with newer Clang versions
    Credits to mustermaxmuller
    
    Signed-off-by: Joe Maples <frap129@gmail.com>
    
    # Conflicts:
    #	driver/runtime/rs_allocation.c

--- driver/runtime/rs_allocation.c
+++ driver/runtime/rs_allocation.c
@@ -350,11 +350,12 @@ ELEMENT_AT(double3)
 ELEMENT_AT(double4)
 
 typedef unsigned long long ull;
-typedef unsigned long long ull2 __attribute__((ext_vector_type(2)));
-typedef unsigned long long ull3 __attribute__((ext_vector_type(3)));
-typedef unsigned long long ull4 __attribute__((ext_vector_type(4)));
+typedef unsigned long ull2 __attribute__((ext_vector_type(2)));
+typedef unsigned long ull3 __attribute__((ext_vector_type(3)));
+typedef unsigned long ull4 __attribute__((ext_vector_type(4)));
 
 #ifndef RS_DEBUG_RUNTIME
+
 SET_ELEMENT_AT_TYPE_IMPL(ull, ulong)
 SET_ELEMENT_AT_TYPE_IMPL(ull2, ulong2)
 SET_ELEMENT_AT_TYPE_IMPL(ull3, ulong3)
@@ -364,6 +365,11 @@ SET_ELEMENT_AT_TYPE_IMPL(ull4, ulong4)
 #undef GET_ELEMENT_AT_TYPE_DEF
 #undef SET_ELEMENT_AT_TYPE_IMPL
 #undef GET_ELEMENT_AT_TYPE_IMPL
+
+SET_ELEMENT_AT_TYPE(ull, ulong)
+
+#undef SET_ELEMENT_AT_TYPE
+#undef GET_ELEMENT_AT_TYPE
 #undef ELEMENT_AT_TYPE
 #endif
 
